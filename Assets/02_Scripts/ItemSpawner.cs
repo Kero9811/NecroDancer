ff7c5851a21cbd083560e54857910c8caa055981
Kero9811 <kero9811@naver.com>
2024-01-16 10:24:12 +0900

    .

---
+++ Assets/02_Scripts/ItemSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] itemPrefabs;
+
+    private void Awake()
+    {
+        spawnPoints = GetComponentsIntChildeExceptMe(spawnPoints);
+    }
+
+    private void Start()
+    {
+        Invoke("Spawn", .5f);
+    }
+
+    void Spawn()
+    {
+        int[] checkList = new int[spawnPoints.Length];
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            int randomNum = Random.Range(1, itemPrefabs.Length + 1);
+
+            if (checkList.Contains(randomNum))
+            {
+                i--;
+                continue;
+            }
+
+            checkList[i] = randomNum;
+            GameObject item = Instantiate(itemPrefabs[randomNum - 1], spawnPoints[i].position, Quaternion.identity);
+            item.transform.SetParent(transform.parent);
+        }
+    }
+
+    private Transform[] GetComponentsIntChildeExceptMe(Transform[] transforms)
+    {
+        transforms = GetComponentsInChildren<Transform>();
+        List<Transform> list = new List<Transform>(transforms);
+        list.RemoveAt(0);
+        transforms = list.ToArray();
+
+        return transforms;
+    }
+}
---
+++ Assets/02_Scripts/MonsterSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] monsterPrefabs;
+    public int monsterCount;
+
+    private void Awake()
+    {
+        spawnPoints = GetComponentsIntChildeExceptMe(spawnPoints);
+    }
+
+    private void Start()
+    {
+        Invoke("Spawn", .5f);
+    }
+
+    void Spawn()
+    {
+        // 랜덤한 포인트에서 스폰이 되는데 해당 포인트에서도 근처 랜덤 좌표로 스폰, 스폰 시 이미 스폰된 좌표가 있으면 다시 랜덤 좌표 선택
+        for (int i = 0; i < monsterCount; i++)
+        {
+            int randomNum = Random.Range(0, monsterPrefabs.Length);
+            GameObject monster = Instantiate(monsterPrefabs[randomNum], spawnPoints[i].position, Quaternion.identity);
+            monster.transform.SetParent(transform.parent);
+
+        }
+    }
+
+    private Transform[] GetComponentsIntChildeExceptMe(Transform[] transforms)
+    {
+        transforms = GetComponentsInChildren<Transform>();
+        List<Transform> list = new List<Transform>(transforms);
+        list.RemoveAt(0);
+        transforms = list.ToArray();
+
+        return transforms;
+    }
+}
